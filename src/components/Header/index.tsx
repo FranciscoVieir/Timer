7f10ad296bd9f280a6226e15e307e4e925acc30d
FranciscoVieir <franciscovieira6397@gmail.com>
2023-01-02 15:18:22 -0300

    feat: add phosphor-react

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import { HeaderContainer } from './styles'
+
+export default function Header() {
+  return (
+    <HeaderContainer>
+      <span>Logo</span>
+      <nav>
+        <a href="">Timer</a>
+        <a href="">History</a>
+      </nav>
+    </HeaderContainer>
+  )
+}
